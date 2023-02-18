a347835c033f01c68f15f6e87c1d2e1e1f957d9f
xen <selegakamil@gmail.com>
2023-02-19 00:04:45 +0100

    Setup the Chrome web driver, open a new window and switch to it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use thirtyfour::prelude::*;
+
+#[tokio::main]
+async fn main() -> WebDriverResult<()> {
+    let mut caps = DesiredCapabilities::chrome();
+    caps.set_debugger_address("localhost:1337")?;
+
+    let driver = WebDriver::new("http://localhost:9515", caps).await?;
+
+    driver.new_window().await?;
+    let handles = driver.windows().await?;
+    driver.switch_to_window(handles[handles.len() - 1].clone()).await?;
+
+    Ok(())
 }
