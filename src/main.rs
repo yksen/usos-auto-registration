7baaf6278584d038f9c07925d5d5ef5e38191bae
xen <selegakamil@gmail.com>
2023-02-19 13:13:37 +0100

    Iterate over open windows and print their titles

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ use thirtyfour::prelude::*;
 async fn main() -> WebDriverResult<()> {
     let mut caps = DesiredCapabilities::chrome();
     caps.set_debugger_address("localhost:1337")?;
-
     let driver = WebDriver::new("http://localhost:9515", caps).await?;
 
-    driver.new_window().await?;
     let handles = driver.windows().await?;
-    driver.switch_to_window(handles[handles.len() - 1].clone()).await?;
+    for handle in handles {
+        driver.switch_to_window(handle).await?;
+        let title = driver.title().await?;
+        println!("Title: {}", title);
+    }
 
     Ok(())
 }
